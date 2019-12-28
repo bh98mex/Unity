9052e964be0869e078ddf70067861b00164f0c34
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-28 00:22:09 +0000

    Unity Multithreading

---
+++ Multithreading/Assets/Threding.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Threding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Start() :: Starting()");
+
+        Thread myThread = new Thread(SlowJob);
+
+        myThread.Start();
+
+    }
+    bool isRunning = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning)
+            Debug.Log("SlowJob isRunning");
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void SlowJob()
+    {
+        isRunning = true;
+        Debug.Log("ExampleScript::SlowJob() -- Doing 100 things, each taking 2 ms");
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(2);    //Sleep for 2ms
+
+        }
+
+        sw.Stop();
+
+        // NOTE: Because of various overheads and context-switches, elapsed time
+        //will be moe than 2 seconds.
+        Debug.Log("Threading :: SlowJob() -- Done! Elapsed time:" + sw.ElapsedMilliseconds / 1000f);
+
+        isRunning = false;
+
+        
+
+    }
+}
---
+++ NavMeshLink/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public float speed;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.updatePosition = false;
+        character.GetComponent<Rigidbody>();
+        speed = 6.0f;
+
+    }
+
+    // Update is called once per frame
+  
+    void Update()
+    {
+
+  
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //move our agent
+                agent.SetDestination(hit.point);
+                speed++;
+            }
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+       
+    }
+    void FixedUpdate()
+    {
+      
+    }
+   
+}
